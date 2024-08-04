687751bfc39541fe1235b3a6554f79223b01e850
Vadzim <shylkovad@gmail.com>
2024-08-04 09:39:25 +0300

    изменения в основных файлах

--- src/main/java/com/example/itexusnew/ItexusNewApplication.java
+++ src/main/java/com/example/itexusnew/ItexusNewApplication.java
@@ -1,13 +1,34 @@
 package com.example.itexusnew;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.task.TaskSchedulingAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration;
+import org.springframework.core.env.Environment;
 
-@SpringBootApplication
+@Slf4j
+@SpringBootApplication(exclude = {
+        WebSocketServletAutoConfiguration.class,
+        TaskSchedulingAutoConfiguration.class
+})
 public class ItexusNewApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ItexusNewApplication.class, args);
+        private final Environment environment;
+
+    public ItexusNewApplication(Environment environment) {
+        this.environment = environment;
     }
 
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(ItexusNewApplication.class);
+        Environment env = application.run(args).getEnvironment();
+        log.info("""
+                        ----------------------------------------------------------
+                        \tApplication '{}' is running! Access URLs:
+                        \tLocal: \t\t{}://localhost:{}{}
+                        ----------------------------------------------------------""",
+        env.getProperty("spring.application.name"), "http", env.getProperty("server.port"), "/user");
+        }
 }
