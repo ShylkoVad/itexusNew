a5b52bf5ace0d36a13316287bc5333bec25d5244
Vadzim <shylkovad@gmail.com>
2024-08-04 09:18:18 +0300

    new

---
+++ src/main/java/com/example/itexusnew/ItexusNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.itexusnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItexusNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItexusNewApplication.class, args);
+    }
+
+}
